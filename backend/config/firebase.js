57955978075ea31dfef7c71821eed33c3f655d9f
Keanno Brennel <keannobrennelamacatangay@iskolarngbayan.pup.edu.ph>
2026-05-18 21:51:28 +0800

    Set up backend entry point and MQTT config

---
+++ backend/config/firebase.js
@@ -0,0 +1,13 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+require("dotenv").config();
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+const db = admin.database();
+const firestore = admin.firestore();
+
+module.exports = { admin, db, firestore };
