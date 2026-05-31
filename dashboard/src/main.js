b89de2c744d67c790b6dbd9760f066f2d9a5b400
Keanno Brennel <keannobrennelamacatangay@iskolarngbayan.pup.edu.ph>
2026-05-31 18:41:53 +0800

    Fix Leaflet marker icons for Vite production build

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -3,6 +3,19 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
 
+// Fix Leaflet marker icons in Vite builds
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
